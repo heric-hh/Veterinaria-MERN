2577f9097b3377c1acb6ec2ff5edfc641a4ddd14
heric-hh <erhh.015@gmail.com>
2024-03-19 14:28:57 -0600

    Incorporando controladores

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,11 @@ import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 
 const app = express();
 dotenv.config();
-
 conectarDb();
 
 app.use("/api/veterinarios", veterinarioRoutes);
+
+
 
 const PORT = process.env.PORT || 4000;
 
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { registrar, perfil } from "../controllers/veterinarioController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Desde /api/veterinarios");
-})
+router.get("/", registrar);
+
+router.get("/perfil", perfil);
+
+router.get("/login", (req, res) => {
+    res.send("Desde /api/veterinarios/login");
+});
 
 export default router;
