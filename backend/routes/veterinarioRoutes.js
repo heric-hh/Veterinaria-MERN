d6e44c479656a21af12058374cc73da97abcda48
heric-hh <erhh.015@gmail.com>
2024-03-21 14:40:55 -0600

    Iniciando seccion de pacientes

--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -1,14 +1,21 @@
 import express from "express";
-import { registrar, perfil, confirmar, autenticar } from "../controllers/veterinarioController.js";
+import { registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from "../controllers/veterinarioController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+//Endpoint publicos
 router.post("/", registrar);
 router.get("/confirmar/:token", confirmar);
 router.post("/login", autenticar);
 
+//Esta ruta envia los datos del perfil para solicitar una nueva password
+router.post("/olvide-password", olvidePassword);
 
+router.route("/olvide-password/:token").get(comprobarToken).post(nuevoPassword);
+
+//Endpoints privados
 router.get("/perfil", checkAuth, perfil);
+
 
 export default router;
