fd3535280d05f654084d96d53e9362ea4d0b4857
heric-hh <erhh.015@gmail.com>
2024-04-09 15:08:26 -0600

    Eliminando Pacientes finalizado. Crud para pacientes completo

--- frontend/src/components/Paciente.jsx
+++ frontend/src/components/Paciente.jsx
@@ -3,16 +3,16 @@ import usePacientes from "../hooks/usePacientes";
 // eslint-disable-next-line react/prop-types
 const Paciente = ({paciente}) => {
 
-    const {setEdicion} = usePacientes();
+    const {setEdicion, eliminarPaciente} = usePacientes();
 
     // eslint-disable-next-line react/prop-types
-    const { email, fecha, nombre, propietario, sintomas } = paciente; 
+    const { email, fecha, nombre, propietario, sintomas , _id} = paciente; 
 
     const formatearFecha = (fecha) => {
         const nuevaFecha = new Date(fecha);
         return new Intl.DateTimeFormat("es-MX", {dateStyle: "long"}).format(nuevaFecha);
     }
-    
+
     return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-8 rounded-xl">
         <p className="font-bold text-indigo-800 py-1">
@@ -50,6 +50,7 @@ const Paciente = ({paciente}) => {
             <button
                 type="button"
                 className="py-2 px-10 bg-red-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+                onClick={() => eliminarPaciente(_id) }
             >Eliminar</button>
 
         </div>
--- frontend/src/context/PacientesProvider.jsx
+++ frontend/src/context/PacientesProvider.jsx
@@ -12,6 +12,26 @@ const PacientesProvider = ({children}) => {
         setPaciente(paciente);
     }
 
+    const eliminarPaciente = async id => {
+        const  confirmar = confirm("¿Confirmas que deseas eliminar?");
+        if(confirmar) {
+            try {
+                const token = localStorage.getItem("token");
+                const config = {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+                const {data} = await clienteAxios.delete(`/pacientes/${id}`, config);
+                const pacientesActualizado = pacientes.filter(pacientesState => pacientesState._id !== id );
+                setPacientes(pacientesActualizado);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
     useEffect(() => {
         const obtenerPacientes = async () => {
             try {
@@ -77,7 +97,8 @@ const PacientesProvider = ({children}) => {
                 pacientes,
                 guardarPaciente,
                 setEdicion,
-                paciente
+                paciente,
+                eliminarPaciente
             }}
         >
             {children}
